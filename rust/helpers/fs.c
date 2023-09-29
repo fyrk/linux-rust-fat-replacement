a34efbd324e0eaa25b615e4f7e814e7d3bcb2a10
Wedson Almeida Filho <walmeida@microsoft.com>
2023-09-29 17:58:11 -0300

    rust: fs: introduce `FileSystem::init_root`
    Allow Rust file systems to specify their root directory. Also allow them
    to create (and do cache lookups of) directory inodes. (More types of
    inodes are added in subsequent patches in the series.)
    
    The `inode::New` type ensures that a new inode is properly initialised
    before it is marked so. It also facilitates error paths by automatically
    marking inodes as failed if they're not properly initialised.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>
    Signed-off-by: Flora Rädiker <flora.raediker@student.hpi.uni-potsdam.de>

--- rust/helpers/fs.c
+++ rust/helpers/fs.c
@@ -5,6 +5,16 @@ struct file *rust_helper_get_file(struct file *f)
 	return get_file(f);
 }
 
+void rust_helper_i_uid_write(struct inode *inode, uid_t uid)
+{
+	i_uid_write(inode, uid);
+}
+
+void rust_helper_i_gid_write(struct inode *inode, gid_t gid)
+{
+	i_gid_write(inode, gid);
+}
+
 struct dentry *rust_helper_dget(struct dentry *dentry)
 {
 	return dget(dentry);
