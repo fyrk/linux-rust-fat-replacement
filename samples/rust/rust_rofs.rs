032e980c73a557eb20a7d5b5bc2a9509fcad1bdb
Wedson Almeida Filho <walmeida@microsoft.com>
2024-01-22 03:39:36 -0300

    rust: fs: introduce `file::Operations::read`
    This allows file systems to customise their behaviour when callers want
    to read from a file.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- samples/rust/rust_rofs.rs
+++ samples/rust/rust_rofs.rs
@@ -4,7 +4,7 @@
 
 use kernel::fs::{dentry, file, file::File, inode, inode::INode, sb::SuperBlock, Offset};
 use kernel::prelude::*;
-use kernel::{c_str, fs, time::UNIX_EPOCH, types::Either, types::Locked};
+use kernel::{c_str, fs, time::UNIX_EPOCH, types::Either, types::Locked, uaccess};
 
 kernel::module_fs! {
     type: RoFs,
@@ -78,6 +78,10 @@ impl file::Operations for RoFs {
 
     fn seek(file: &File<Self>, offset: Offset, whence: file::Whence) -> Result<Offset> {
         file::generic_seek(file, offset, whence)
+    }
+
+    fn read(_: &File<Self>, _: &mut uaccess::UserSliceWriter, _: &mut Offset) -> Result<usize> {
+        Err(EISDIR)
     }
 
     fn read_dir(
