36f3871caeea4db8b001994f0cde2865eb5bfef1
Wedson Almeida Filho <walmeida@microsoft.com>
2024-01-22 04:51:47 -0300

    rust: fs: introduce `DEntry<T>`
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- rust/helpers/fs.c
+++ rust/helpers/fs.c
@@ -5,6 +5,11 @@ struct file *rust_helper_get_file(struct file *f)
 	return get_file(f);
 }
 
+struct dentry *rust_helper_dget(struct dentry *dentry)
+{
+	return dget(dentry);
+}
+
 loff_t rust_helper_i_size_read(const struct inode *inode)
 {
 	return i_size_read(inode);
